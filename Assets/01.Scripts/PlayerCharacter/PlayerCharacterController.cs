91b01b7cf6332b8795814d81e9b39dcae0d266cd
Komorio <twilivez@gmail.com>
2020-05-21 11:54:34 +0900

    [ADD] 기본 PlayerCharacterController 추가

---
+++ Assets/01.Scripts/PlayerCharacter/PlayerCharacterController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCharacterController : MonoBehaviour
+{
+    [Header("Values")]
+    [SerializeField]
+    private float defaultSpeed;
+    private float speed;
+
+    private void Awake(){
+        speed = defaultSpeed;
+    }
+
+    private void Update(){
+        if(Input.anyKey){
+            if(Input.GetKey(KeyCode.A)){
+                Move(Vector2.left);
+            }
+
+            if(Input.GetKey(KeyCode.D)){
+                Move(Vector2.right);
+            }
+
+            if(Input.GetKeyDown(KeyCode.W)){
+                Apply();
+            }
+
+            if(Input.GetKeyDown(KeyCode.E)){
+                Cancel();
+            }
+        }
+    }
+    
+    private void Move(Vector2 direction){
+        gameObject.transform.Translate(direction * speed * Time.deltaTime);
+    }
+
+    private void Interaction() { }
+    private void Apply() { }
+    private void Cancel() { }
+}
