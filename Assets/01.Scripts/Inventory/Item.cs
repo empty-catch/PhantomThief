263ce42b3b763d2d707f68b30313b0e3ef359d2c
박창용 <tempus157@gmail.com>
2020-05-28 18:31:32 +0900

    [ADD] 인벤토리 아이템 추가 및 선택 기능 추가

---
+++ Assets/01.Scripts/Inventory/Item.cs
@@ -0,0 +1,29 @@
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class Item : MonoBehaviour, IPointerDownHandler
+{
+    public event Action Select;
+
+    [SerializeField]
+    private Image image;
+    [SerializeField]
+    private Image selection;
+    [SerializeField]
+    private Sprite active;
+    [SerializeField]
+    private Sprite inactive;
+
+    public void SetActive(bool value)
+    {
+        image.sprite = value ? active : inactive;
+        selection.color = new Color(1F, 1F, 1F, value ? 1F : 0F);
+    }
+
+    public void OnPointerDown(PointerEventData _)
+    {
+        Select?.Invoke();
+    }
+}
