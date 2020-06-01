a3aba988e3b8ff07523482005dd803ddd17a8536
Komorio <twilivez@gmail.com>
2020-06-01 09:58:20 +0900

    [ADD] NPC Scriptable 추가

---
+++ Assets/01.Scripts/ScriptableObject/NPCScriptable.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using System;
+
+[Serializable]
+public class NPCDialog{
+    public string dialog;
+    public int targetIndex;
+}
+
+
+[CreateAssetMenu(fileName = "NPCScriptable", menuName = "NPCScriptable", order = 0)]
+public class NPCScriptable : ScriptableObject {
+    [SerializeField]
+    private NPCDialog[] dialogs;
+
+    public NPCDialog[] Dialogs => dialogs;
+}
