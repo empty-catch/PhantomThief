f6d61ac56c0dfa107d89f5ea296be9e31e6de879
Komorio <twilivez@gmail.com>
2020-06-01 09:17:39 +0900

    [ADD] Camera 위치 설정 해주는 Script 추가

---
+++ Assets/01.Scripts/Manager/CameraController.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField]
+    private Transform targetObject;
+
+    private Vector3 locationMove;
+
+    private void Start(){
+        locationMove.y = targetObject.position.y + 4;
+        locationMove.z = -150;
+    }
+
+    private void Update(){
+        locationMove.x = targetObject.position.x;
+        gameObject.transform.position = locationMove;
+    }
+}
