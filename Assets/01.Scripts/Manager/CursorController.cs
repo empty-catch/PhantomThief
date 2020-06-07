c9ab02111ed8efb4f91d2ca38f139a6f029e2d05
Komorio <twilivez@gmail.com>
2020-06-08 08:24:25 +0900

    [ADD] 상태에 따라 Cursor 변경

---
+++ Assets/01.Scripts/Manager/CursorController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorController : MonoBehaviour
+{
+    [Header("Cursor Texture")]
+    [SerializeField]
+    private Texture2D[] cursorSprites;
+    private Ray mouseRay;
+    
+    private void Awake(){
+        mouseRay.direction = Vector2.zero;
+    }
+
+    private void Update(){
+        // interactive state
+        if(GetCurrentMouseState()){
+            Cursor.SetCursor(cursorSprites[0], Vector2.zero, CursorMode.Auto);
+        } else {
+            Cursor.SetCursor(cursorSprites[1], Vector2.zero, CursorMode.Auto);
+        }
+    }
+
+    private bool GetCurrentMouseState(){
+        mouseRay.origin = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        RaycastHit2D hit;
+        hit = Physics2D.Raycast(mouseRay.origin, mouseRay.direction, Mathf.Infinity, LayerMask.GetMask("InteractionObject"));
+
+        return hit.collider != null;
+    }
+}
