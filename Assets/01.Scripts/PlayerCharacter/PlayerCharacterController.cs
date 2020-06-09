fd316fe7ed7bf6a316562bde184e7660af314d25
Komorio <twilivez@gmail.com>
2020-06-10 01:51:00 +0900

    [UPDATE] 기본적인 UI 적용

--- Assets/01.Scripts/PlayerCharacter/PlayerCharacterController.cs
+++ Assets/01.Scripts/PlayerCharacter/PlayerCharacterController.cs
@@ -82,8 +82,14 @@ public class PlayerCharacterController : MonoBehaviour
         gameObject.transform.localScale = direction;
     }
     
-    private void Move(Vector2 direction){       
-        gameObject.transform.Translate(direction * speed * Time.deltaTime);
+    private void Move(Vector2 direction){
+        Vector2 position = (Vector2)gameObject.transform.position + direction * speed * Time.deltaTime;
+
+        if(Mathf.Abs(position.x) > 12.3f){
+            gameObject.transform.position = new Vector2(12.3f, 0) * direction;
+        } else{
+            gameObject.transform.position = position;
+        }
     }
 
     private void Interaction(){                
